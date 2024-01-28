77b44814e6dce17fd014c10f22e40513694116be
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-28 12:13:52 +0200

    Update fetchApi.js

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,27 +1,31 @@
 import axios from "axios";
-const baseUrl = "https://api.themoviedb.org/3/"
-const token = process.env.REACT_APP_TMDB_TOKEN || '';
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const apiKey = "3fac70260a5a4a0cc9324207af7f9c8c"; // Replace with your actual TMDB API key
 
 let headers = {
-  Authorization: `Bearer ${token}`,
-  'Content-Type': 'application/json;charset=utf-8'
-}
+  Authorization: `Bearer ${apiKey}`,
+  'Content-Type': 'application/json;charset=utf-8',
+};
+
 const fetchApi = (data) => {
   return axios.get(
-    `${baseUrl}${data.path}`, 
+    `${baseUrl}${data.path}`,
     { 
       headers,
-      params : data?.params
+      params: data?.params,
     }
-  )
-}
+  );
+};
+
 export const postFetchApi = (data) => {
-  return axios.post( 
+  return axios.post(
     `${baseUrl}${data.path}`,
     data?.params,
     {
       headers,
     }
-  )
-}
-export default fetchApi
+  );
+};
+
+export default fetchApi;
