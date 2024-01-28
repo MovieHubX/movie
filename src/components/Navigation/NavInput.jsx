85661e7de4792d088fe466f12559e230c2991279
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-28 19:02:04 +0200

    Update NavInput.jsx

--- src/components/Navigation/NavInput.jsx
+++ src/components/Navigation/NavInput.jsx
@@ -1,123 +1,120 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Box, Input } from "@chakra-ui/react";
-import React, { memo, useRef, useState } from "react";
 import { Search2Icon } from "@chakra-ui/icons";
+import { Box, Input } from "@chakra-ui/react";
+import React, { memo, useCallback, useEffect, useRef, useState, useTransition } from "react";
 import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
-import { multiSearch } from "../../services/searchSlice";
+import {
+  multiSearch
+} from "../../services/searchSlice";
 import SearchTopKeyWordsList from "./SearchTopKeyWordsList";
 
 const NavInput = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const searchInput = useRef(null);
+  const [, startTransition] = useTransition()
   const [searchText, setSearchText] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-
-  const handleSearchTextChange = async (e) => {
+  const [queryText, setQueryText] = useState('')
+  const [isShow, setIsShow] = useState(false);
+  const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
-
-    // Perform search and update search results
-    const results = await dispatch(
-      multiSearch({
-        path: "search/multi",
-        params: {
-          query: e.target.value,
-        },
-      })
-    );
-
-    setSearchResults(results); // Update search results
+    startTransition(() => {
+      setQueryText(e.target.value)
+      dispatch(
+        multiSearch({
+          path: "search/multi",
+          params: {
+            query: e.target.value,
+          },
+        })
+      );
+      setIsShow(true)
+    })
   };
+  useEffect(() => {
+    setIsShow(document.activeElement.tagName === "INPUT")
+  }, [document.activeElement.tagName])
 
-  const handleSearchWithKeyWord = async () => {
-    // Perform search and update search results
-    const results = await dispatch(
-      multiSearch({
-        path: "search/multi",
-        params: {
-          query: searchText,
-        },
-      })
-    );
-
-    setSearchResults(results); // Update search results
-  };
+  const handleSearchWithKeyWord = useCallback((text = queryText) => {
+    if (text) {
+      dispatch(
+        multiSearch({
+          path: "search/multi",
+          params: {
+            query: text,
+          },
+        })
+      );
+      // navigate to search page
+      setSearchText(text);
+      navigate(`/search`)
+      searchInput.current.blur();
+      setIsShow(false)
+    }
+  }, [queryText]);
 
   const handlePressEnter = (e) => {
     if (e.key === "Enter") {
       handleSearchWithKeyWord();
     }
   };
-
   return (
-    <>
-      <Box
-        width={{
-          base: "55%",
-          lg: "40%",
-        }}
-        position="relative"
-        ml="auto"
-        mr={{ lg: "10px" }}
-      >
-        <Box position="relative" overflow="hidden">
-          <Input
-            variant="flushed"
-            autoCapitalize="off"
-            position="relative"
-            focusBorderColor="primaryColor"
-            placeholder="-_-"
-            _placeholder={{
-              color: "decsColor",
-            }}
-            value={searchText}
-            onChange={(e) => handleSearchTextChange(e)}
-            fontSize={{
-              base: "sm",
-              md: "md",
-            }}
-            w="full"
-            pr="40px"
-            pl="5px"
-            onKeyDown={(e) => handlePressEnter(e)}
-          />
-          {/* search icon */}
-          <Box
-            position="absolute"
-            right="15px"
-            top="50%"
-            transform="translateY(-50%)"
-            zIndex={500}
-            fontSize="20px"
-            color="textColor"
-            cursor={"pointer"}
-            onClick={() => handleSearchWithKeyWord()}
-          >
+    <Box
+      w={{
+        base: "55%",
+        lg: "40%",
+      }}
+      position="relative"
+      ml="auto"
+      mr={{ lg: "50px" }}
+    >
+      <Box position="relative" overflow="hidden">
+        <Input
+          variant="flushed"
+          autoCapitalize="off"
+          position="relative"
+          focusBorderColor="primaryColor"
+          placeholder="-_-"
+          _placeholder={{
+            color: "decsColor",
+          }}
+          value={searchText}
+          onChange={(e) => handleSearchTextChange(e)}
+          ref={searchInput}
+          fontSize={{
+            base: "sm",
+            md: "md",
+          }}
+          w="full"
+          pr="40px"
+          pl="5px"
+          onKeyDown={(e) => handlePressEnter(e)}
+        />
+        {/* search icon */}
+        <Box
+          position="absolute"
+          right="15px"
+          top="50%"
+          transform="translateY(-50%)"
+          zIndex={500}
+          fontSize="20px"
+          color="textColor"
+          cursor={"pointer"}
+          onClick={() => handleSearchWithKeyWord()}
+        >
+          <Link to="/search">
             <Search2Icon />
-          </Box>
+          </Link>
         </Box>
       </Box>
-
-      {/* Below the nav bar, the separate search bar */}
-      <Box
-        w="100%" // Full width
-        position="fixed"
-        top="60px" // Adjust the top position based on your nav bar's height
-        bg="white" // Adjust the background color as needed
-        boxShadow="sm" // Add shadow as needed
-        zIndex="999" // Ensure it's above other elements
-        p="2"
-      >
-        {/* Your separate search bar content */}
+      {isShow && (
         <SearchTopKeyWordsList
-          searchResults={searchResults}
-          handleClickListKeyWords={(text) => {
-            setSearchText(text);
-            handleSearchWithKeyWord();
-          }}
+          handleClickListKeyWords={handleSearchWithKeyWord}
         />
-      </Box>
-    </>
+      )}
+    </Box>
   );
 };
 
