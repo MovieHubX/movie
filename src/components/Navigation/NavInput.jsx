f861a2c93f5e90515cf9d443722614b5efef933a
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-28 16:49:44 +0200

    Update NavInput.jsx

--- src/components/Navigation/NavInput.jsx
+++ src/components/Navigation/NavInput.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { Search2Icon } from "@chakra-ui/icons";
 import { Box, Input } from "@chakra-ui/react";
-import React, { memo, useCallback, useEffect, useRef, useState } from "react";
+import React, { memo, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -11,17 +10,15 @@ import SearchTopKeyWordsList from "./SearchTopKeyWordsList";
 const NavInput = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const searchInput = useRef(null);
-  const [searchText, setSearchText] = useState('');
-  const [queryText, setQueryText] = useState('');
-  const [isShow, setIsShow] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
 
   const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
   };
 
-  const handleSearchWithKeyWord = useCallback(() => {
-    const text = queryText.trim();
+  const handleSearchWithKeyWord = () => {
+    const text = searchText.trim();
     if (text) {
       dispatch(
         multiSearch({
@@ -31,28 +28,21 @@ const NavInput = () => {
           },
         })
       );
-      setSearchText(text);
+      setSearchText("");
       navigate(`/search`);
-      searchInput.current.blur();
-      setIsShow(false);
-    }
-  }, [queryText, dispatch, navigate]);
-
-  const handlePressEnter = (e) => {
-    if (e.key === "Enter") {
-      handleSearchWithKeyWord();
+      setIsSearchBarVisible(false);
     }
   };
 
   const toggleSearchBar = () => {
-    setIsShow(!isShow);
+    setIsSearchBarVisible(!isSearchBarVisible);
   };
 
   return (
     <Box
       display="flex"
       alignItems="center"
-      justifyContent="flex-end" // Align to the right
+      justifyContent="flex-end"
       ml={{ lg: "50px" }}
     >
       {/* Search icon */}
@@ -60,9 +50,8 @@ const NavInput = () => {
         fontSize="20px"
         color="textColor"
         cursor="pointer"
+        marginRight="15px"
         onClick={toggleSearchBar}
-        zIndex={500}
-        marginRight="15px" // Add margin to create a gap
       >
         <Link to="/search">
           <Search2Icon />
@@ -70,47 +59,51 @@ const NavInput = () => {
       </Box>
 
       {/* Search input */}
-      {isShow && (
-        <Box>
-          <Input
-            variant="flushed"
-            autoCapitalize="off"
-            focusBorderColor="primaryColor"
-            placeholder="-_-"
-            _placeholder={{
-              color: "decsColor",
-            }}
-            value={searchText}
-            onChange={(e) => handleSearchTextChange(e)}
-            ref={searchInput}
-            fontSize={{
-              base: "sm",
-              md: "md",
-            }}
-            pr="40px"
-            pl="5px"
-            onKeyDown={(e) => handlePressEnter(e)}
-          />
-          {/* Search icon within the input */}
-          <Box
-            position="absolute"
-            right="15px"
-            top="50%"
-            transform="translateY(-50%)"
-            zIndex={500}
-            fontSize="20px"
-            color="textColor"
-            cursor="pointer"
-            onClick={handleSearchWithKeyWord}
-          >
-            <Link to="/search">
-              <Search2Icon />
-            </Link>
-          </Box>
+      <Box
+        position="relative"
+        width={isSearchBarVisible ? "200px" : "0"}
+        overflow="hidden"
+        transition="width 0.3s ease"
+      >
+        <Input
+          variant="flushed"
+          autoCapitalize="off"
+          focusBorderColor="primaryColor"
+          placeholder="-_-"
+          _placeholder={{
+            color: "decsColor",
+          }}
+          value={searchText}
+          onChange={(e) => handleSearchTextChange(e)}
+          fontSize={{
+            base: "sm",
+            md: "md",
+          }}
+          pr="40px"
+          pl="5px"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSearchWithKeyWord();
+          }}
+        />
+        {/* Search icon within the input */}
+        <Box
+          position="absolute"
+          right="15px"
+          top="50%"
+          transform="translateY(-50%)"
+          zIndex={500}
+          fontSize="20px"
+          color="textColor"
+          cursor="pointer"
+          onClick={handleSearchWithKeyWord}
+        >
+          <Link to="/search">
+            <Search2Icon />
+          </Link>
         </Box>
-      )}
+      </Box>
 
-      {isShow && (
+      {isSearchBarVisible && (
         <SearchTopKeyWordsList
           handleClickListKeyWords={handleSearchWithKeyWord}
         />
