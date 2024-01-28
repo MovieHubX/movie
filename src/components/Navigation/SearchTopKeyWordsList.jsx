7c4902c2e9440a2e4d72bd4e4d86e4c73daa606b
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-28 16:16:15 +0200

    Update SearchTopKeyWordsList.jsx

--- src/components/Navigation/SearchTopKeyWordsList.jsx
+++ src/components/Navigation/SearchTopKeyWordsList.jsx
@@ -1,5 +1,5 @@
 import {
-  Box, Divider, Heading, ListItem, OrderedList, Text
+  Box, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverBody, Divider, Heading, ListItem, OrderedList, Text
 } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
@@ -9,59 +9,60 @@ const SearchTopKeyWordsList = ({ handleClickListKeyWords }) => {
   const data = useSelector(multiSearchSelector);
   if(data.length > 0) {
     return (
-      <Box
-        w="full"
-        position="absolute"
-        left="0"
-        right="0"
-        bg="#384e7b"
-        transform={"translateY(7px)"}
-        px="5px"
-        py="10px"
-        pl={'0'}
-        shadow='lg'
-        zIndex="100"
-        rounded="5px"
-        boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px"
-      >
-        <OrderedList fontSize="14px" fontWeight="light">
-          {data?.map((item, i) => {
-            if(!item.title) return null;
-            return (
-              <ListItem
-                key={item.id}
-                display="flex"
-                alignItems="center"
-                cursor="pointer"
-                columnGap={"10px"}
-                _notLast={{ marginBottom: "5px" }}
-                onClick={() => handleClickListKeyWords(item.title)}
-                _hover={{
-                  color: "primaryColor",
-                }}
-                w="full"
-              >
-                <Text
-                  overflow="hidden"
-                  textOverflow="ellipsis"
-                  whiteSpace="nowrap"
-                  cursor="pointer"
-                >
-                  {item.title}
-                </Text>
-              </ListItem>
-            );
-          })}
-          {
-            data?.length === 0 &&
-            <Text
-              ml='-10px'
-            >
-              No result, please try something else.
-            </Text>
-          }
-        </OrderedList>
-      </Box>
+      <Popover placement="bottom-start">
+        <PopoverTrigger>
+          <Box
+            cursor="pointer"
+            position="absolute"
+            left="0"
+            right="0"
+            transform={"translateY(7px)"}
+            zIndex="100"
+            rounded="5px"
+          >
+            <Heading as="h6" size="sm" color="white" p="2" textAlign="center">
+              Search Suggestions
+            </Heading>
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverBody>
+            <OrderedList fontSize="14px" fontWeight="light">
+              {data?.map((item, i) => {
+                if (!item.title) return null;
+                return (
+                  <ListItem
+                    key={item.id}
+                    display="flex"
+                    alignItems="center"
+                    cursor="pointer"
+                    columnGap={"10px"}
+                    _notLast={{ marginBottom: "5px" }}
+                    onClick={() => handleClickListKeyWords(item.title)}
+                    _hover={{
+                      color: "primaryColor",
+                    }}
+                    w="full"
+                  >
+                    <Text
+                      overflow="hidden"
+                      textOverflow="ellipsis"
+                      whiteSpace="nowrap"
+                      cursor="pointer"
+                    >
+                      {item.title}
+                    </Text>
+                  </ListItem>
+                );
+              })}
+              {data?.length === 0 && (
+                <Text ml="-10px">No result, please try something else.</Text>
+              )}
+            </OrderedList>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
     );
   }
 };
