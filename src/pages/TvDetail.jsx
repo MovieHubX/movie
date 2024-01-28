21ed183aaaa88cf2c311362b61cdd6bdbeb00f5e
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-28 15:57:50 +0200

    Update TvDetail.jsx

--- src/pages/TvDetail.jsx
+++ src/pages/TvDetail.jsx
@@ -1,48 +1,46 @@
-import { Box, Center, Heading, Image, Text } from "@chakra-ui/react";
-import React, { useCallback, useEffect, useRef } from "react";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import { useDispatch, useSelector } from "react-redux";
-import { Autoplay, Keyboard, Lazy, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import { StarIcon } from "@chakra-ui/icons";
 import {
+  Box,
   Breadcrumb,
   BreadcrumbItem,
-  Button, // Removed Button from Chakra UI import, as we are using the custom Button from this code
+  Center,
   Flex,
+  Heading,
+  Text,
+  Button,
+  Image,
   Stack,
 } from "@chakra-ui/react";
-
+import React, { useEffect, useRef, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { FaYoutube } from "react-icons/fa"; // Import FaYoutube icon
 
-import moment from "moment/moment";
-import ReactPlayer from "react-player";
 import ListFilmLayout from "../components/Layout/ListFilmLayout";
 import Loading from "../components/Loading/Loading";
+import VideoPlayer from "../components/VideoPlayer/VideoPlayer";
+
 import { getConfigSelector, tvDetailSelector } from "../redux/selector";
 import { getTvDetail } from "../services";
+import moment from "moment/moment";
 
 export const TvDetail = () => {
   const dispatch = useDispatch();
   const player = useRef();
   const params = useParams();
   const { id } = params;
+
   const { tvDetail, status } = useSelector(tvDetailSelector);
   const { config } = useSelector(getConfigSelector);
-  const dateFormated = moment(tvDetail?.release_date).format("YYYY");
 
   const handleFindTrailerKey = useCallback(() => {
-    // get youtube trailer key of not have trailer get first video
     const youtubeVideos = tvDetail?.videos?.results?.filter(
       (item) => item?.site === "YouTube"
     );
     const trailer = youtubeVideos?.find((item) => item?.type === "Trailer");
-    if (trailer) return trailer.key;
-    return youtubeVideos?.videos?.results[0]?.key;
+    return trailer?.key || (youtubeVideos?.length > 0 && youtubeVideos[0]?.key);
   }, [tvDetail]);
+
   const trailerKey = handleFindTrailerKey();
 
   useEffect(() => {
@@ -54,13 +52,11 @@ export const TvDetail = () => {
     );
   }, [id]);
 
-  useEffect(() => {}, [tvDetail]);
-
   return (
     <Box mt={"50px"}>
-      {tvDetail?.seasons?.length > 0 && status === "done" ? (
+      {status === "done" ? (
         <Box>
-          {/* info  */}
+          {/* info */}
           <Box
             color={"decsColor"}
             _notLast={{
@@ -69,13 +65,12 @@ export const TvDetail = () => {
               },
             }}
           >
-            {/* date */}
             <Box>
               <Breadcrumb
                 separator={"  -  "}
                 fontSize={{
-                  base: "2xl",
-                  lg: "4xl",
+                  base: "xl",
+                  lg: "2xl",
                 }}
                 color="textColor"
                 fontWeight="bold"
@@ -87,23 +82,56 @@ export const TvDetail = () => {
                   </Text>
                 </BreadcrumbItem>
                 <BreadcrumbItem>
-                  <Box>{dateFormated}</Box>
+                  <Box>
+                    {moment(tvDetail?.release_date).format("YYYY")}
+                  </Box>
                 </BreadcrumbItem>
               </Breadcrumb>
 
               <Flex align={"center"}>
-                <Text
-                  color="primaryColor"
-                  lineHeight={"0"}
-                  fontWeight="bold"
-                  fontSize={"18px"}
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  border="1px solid"
+                  borderColor="primaryColor"
+                  p="2"
+                  borderRadius="md"
+                  mr="4"
                 >
-                  {tvDetail?.vote_average.toFixed(1)}
-                </Text>
-                <StarIcon color="yellow" ml="5px" />
+                  <Text
+                    color="primaryColor"
+                    fontWeight="bold"
+                    fontSize="18px"
+                    pr="2"
+                    lineHeight="1.3"
+                  >
+                    {tvDetail?.vote_average.toFixed(1)}
+                  </Text>
+                  <StarIcon color="yellow" boxSize="1em" />
+                </Box>
+
+                {trailerKey && (
+                  <Box ml="4">
+                    <Button
+                      onClick={() => {
+                        const trailerUrl = `https://www.youtube.com/watch?v=${trailerKey}`;
+                        window.open(trailerUrl, "_blank");
+                      }}
+                      variant="outline"
+                      colorScheme="red"
+                      border="1px solid"
+                      borderColor="red.500"
+                      borderRadius="md"
+                      px="3"
+                    >
+                      <FaYoutube style={{ marginRight: "5px" }} />
+                      Watch Trailer
+                    </Button>
+                  </Box>
+                )}
               </Flex>
             </Box>
-            {/* overview */}
+
             <Box
               fontSize={{
                 base: "sm",
@@ -113,125 +141,74 @@ export const TvDetail = () => {
             >
               <Text>{tvDetail?.overview}</Text>
             </Box>
-            {/* area & genres */}
-            <Box
-              fontSize={{
-                base: "xs",
-                md: "sm",
-                lg: "lg",
-              }}
-            >
-              <Flex align="center">
-                <Text color={"textColor"} mr="10px">
-                  Area :
-                </Text>
-                <Breadcrumb
-                  separator=","
-                  spacing="3px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="flex-start"
-                >
-                  {tvDetail?.spoken_languages?.map((item, i) => (
-                    <BreadcrumbItem key={i}>
-                      <Box>{item?.name}</Box>
+
+            <Flex align="center">
+              <Box mr="10px" color={"textColor"}>
+                Genre :
+              </Box>
+              <Breadcrumb separator="," spacing="3px">
+                {tvDetail?.genres?.map((item) => {
+                  return (
+                    <BreadcrumbItem key={item.id}>
+                      <Link to={`genres/${item.id}`}>{item.name}</Link>
                     </BreadcrumbItem>
-                  ))}
-                </Breadcrumb>
-              </Flex>
-              <Flex align="center">
-                <Text mr="10px" color={"textColor"}>
-                  Types :
-                </Text>
-                <Breadcrumb separator="," spacing="3px">
-                  {tvDetail?.genres?.map((item) => {
-                    return (
-                      <BreadcrumbItem key={item.id}>
-                        <Link to={`genres/${item.id}`}>{item.name}</Link>
-                      </BreadcrumbItem>
-                    );
-                  })}
-                </Breadcrumb>
-              </Flex>
-            </Box>
+                  );
+                })}
+              </Breadcrumb>
+            </Flex>
           </Box>
-          {/* Trailer */}
-          <Box
-            overflow={"hidden"}
-            w="full"
-            mb="50px"
-            h={{ base: "60vw", md: "80vh" }}
-          >
-            {trailerKey ? (
-              <ReactPlayer
-                ref={player}
-                url={`https://www.youtube.com/watch?v=${handleFindTrailerKey()}`}
-                width="100%"
-                height="100%"
-                controls
-              />
-            ) : (
-              <Image
-                src={`${config?.images?.base_url}/original/${tvDetail?.backdrop_path}`}
-                alt={`${tvDetail?.title || tvDetail?.name} poster`}
-                objectFit="cover"
-              />
-            )}
-          </Box>
+
           {/* season */}
+ {/* season */}
           <Box>
             {tvDetail?.seasons?.map((item, i) => {
-              const seasonDateFormated = moment(item?.air_date).format(
-                "MMMM Do YYYY"
-              );
+              const seasonDateFormated = moment(item?.air_date).format("MMMM Do YYYY");
               return (
                 <Flex
                   key={i}
-                  direction={{ base: "column", sm: "row" }}
-                  overflow="hidden"
-                  variant="outline"
-                  borderWidth="1px"
-                  borderRadius="lg"
-                  p="4"
-                  mb="8"
-                  alignItems={"start"}
-                  columnGap="8"
-                  minH={"300px"}
+                  direction={{ base: 'column', sm: 'row' }}
+                  overflow='hidden'
+                  variant='outline'
+                  borderWidth='1px'
+                  borderRadius='lg'
+                  p='4'
+                  mb='8'
+                  alignItems={'start'}
+                  columnGap='8'
+                  minH={'300px'}
                 >
                   <Image
-                    objectFit="cover"
-                    display={{ base: "none", md: "block" }}
-                    maxW={{ base: "100%", sm: "200px" }}
+                    objectFit='cover'
+                    display={{ base: 'none', md: 'block' }}
+                    maxW={{ base: '100%', sm: '200px' }}
                     src={`${config?.images?.base_url}/original/${item.poster_path}`}
                     alt={`${item.name} poster`}
                   />
 
-                  <Stack flexGrow={1} minH={"300px"}>
-                    <Stack flexGrow={1} mt="4">
-                      <Heading size="lg" display={"inline-block"} mb="2">
-                        {item.name} - {seasonDateFormated || ""}
+                  <Stack flexGrow={1} minH={'300px'}>
+                    <Stack flexGrow={1} mt='4'>
+                      <Heading
+                        size='lg' display={'inline-block'} mb='2'
+                      >
+                        {item.name} - {seasonDateFormated || ''}
                       </Heading>
-                      <Heading size={"md"} mb={"6"}>
+                      <Heading size={'md'} mb={'6'}>
                         {item.episode_count} episodes
                       </Heading>
-                      <Text flexGrow={1} h="full" py="2" color={"decsColor"}>
-                        {item.overview ||
-                          `${item.name} of ${
-                            tvDetail?.title || tvDetail?.name
-                          } premiered on ${seasonDateFormated || "N/A"}`}
+                      <Text flexGrow={1} h='full' py='2' color={'decsColor'}>
+                        {item.overview || `${item.name} of ${tvDetail?.title || tvDetail?.name} premiered on ${seasonDateFormated || 'N/A'}`}
                       </Text>
                     </Stack>
-                    <Box display={"block"}>
+                    <Box display={'block'}>
                       <Link to={`/tv/${id}/season/${item.season_number}`}>
-                        {/* Use custom Button from this code, and colorScheme='blue' */}
-                        <Button mb="6" variant="solid" colorScheme="blue">
+                        <Button mb='6' variant='solid' colorScheme='blue'>
                           Watch Now
                         </Button>
                       </Link>
                     </Box>
                   </Stack>
                 </Flex>
-              );
+              )
             })}
           </Box>
 
@@ -239,7 +216,7 @@ export const TvDetail = () => {
           {tvDetail?.recommendations?.results?.length > 0 && (
             <Box>
               <Heading fontSize="2xl" mt="50px">
-                You may Like
+                Similar
               </Heading>
               <ListFilmLayout
                 listFilm={tvDetail?.recommendations?.results}
