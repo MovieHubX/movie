b90f33cb58f36ad1f9e24a8ef52883b4251bbe1f
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-29 19:35:59 +0200

    Update NavInput.jsx

--- src/components/Navigation/NavInput.jsx
+++ src/components/Navigation/NavInput.jsx
@@ -140,7 +140,7 @@ const NavInput = () => {
       <Box
         position="absolute"
         top="55px"  // Adjust the top position based on your navigation bar height
-        left="0"
+        right="0"   // Align to the rightmost part
         bg="rgba(21, 31, 50, 1)"
         zIndex={999}  // Lower zIndex to appear below navigation bar
         p="10px"
