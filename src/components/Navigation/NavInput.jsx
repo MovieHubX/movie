8e908e26e2198812b74a9e4285bf73eef776bab6
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-29 19:20:35 +0200

    Update NavInput.jsx

--- src/components/Navigation/NavInput.jsx
+++ src/components/Navigation/NavInput.jsx
@@ -1,4 +1,13 @@
-// ... (import statements)
+import { Search2Icon } from "@chakra-ui/icons";
+import { Box, Input } from "@chakra-ui/react";
+import React, { memo, useCallback, useEffect, useRef, useState, useTransition } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  multiSearch
+} from "../../services/searchSlice";
+import SearchTopKeyWordsList from "./SearchTopKeyWordsList";
 
 const NavInput = () => {
   const dispatch = useDispatch();
@@ -61,20 +70,20 @@ const NavInput = () => {
     const handleEscapeKey = (e) => {
       if (e.key === "Escape") {
         setIsShow(false);
-        setIsSearchOpen(false);
+        setIsSearchOpen(false); // Close search bar on escape key
       }
     };
 
     const handleOutsideClick = (e) => {
       if (!searchInput.current.contains(e.target)) {
         setIsShow(false);
-        setIsSearchOpen(false);
+        setIsSearchOpen(false); // Close search bar on outside click
       }
     };
 
     const handleInputFocus = () => {
       setIsShow(true);
-      setIsSearchOpen(true);
+      setIsSearchOpen(true); // Open search bar on input focus
     };
 
     const handleInputBlur = () => {
@@ -97,12 +106,13 @@ const NavInput = () => {
   return (
     <Box
       w="100%"
-      position="fixed"  // Fixed positioning
-      top="0"  // Always at the top
+      position="absolute"
+      top="0"
+      left="0"
       bg="rgba(21, 31, 50, 1)"
       zIndex={1000}
       p="10px"
-      transition="top 0.3s ease-in-out"
+      transition="top 0.3s ease-in-out"  // Add transition effect
     >
       <Box position="relative" overflow="hidden">
         <Input
