be74cb40576ac46cc4d099a8c978d0b6416cfc57
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-29 19:39:50 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,91 +1,199 @@
-import React, { memo } from "react";
-import { Link } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Search2Icon } from "@chakra-ui/icons";
+import { Box, Input } from "@chakra-ui/react";
+import React, { memo, useCallback, useEffect, useRef, useState, useTransition } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import {
-  Box,
-  Flex, HStack, Stack
-} from "@chakra-ui/react";
-import MenuMobile from "./MenuMobile";
-import NavInput from "./NavInput";
-import { navs } from "../Constans";
+  multiSearch
+} from "../../services/searchSlice";
+import SearchTopKeyWordsList from "./SearchTopKeyWordsList";
 
-const Menu = () => {
+const NavInput = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchInput = useRef(null);
+  const [, startTransition] = useTransition();
+  const [searchText, setSearchText] = useState('');
+  const [queryText, setQueryText] = useState('');
+  const [isShow, setIsShow] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleSearchTextChange = (e) => {
+    setSearchText(e.target.value);
+    startTransition(() => {
+      setQueryText(e.target.value);
+      dispatch(
+        multiSearch({
+          path: "search/multi",
+          params: {
+            query: e.target.value,
+          },
+        })
+      );
+      setIsShow(true);
+    });
+  };
+
+  const handleSearchWithKeyWord = useCallback((text = queryText) => {
+    if (text) {
+      dispatch(
+        multiSearch({
+          path: "search/multi",
+          params: {
+            query: text,
+          },
+        })
+      );
+      // navigate to search page
+      setSearchText(text);
+      navigate(`/search`);
+      searchInput.current.blur();
+      setIsShow(false);
+      setIsSearchOpen(false); // Close search bar after search
+    } else {
+      // If search bar is empty, focus on the input field
+      searchInput.current.focus();
+    }
+  }, [queryText, navigate]);
+
+  const handlePressEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSearchWithKeyWord();
+    }
+  };
+
+  const handleSearchIconClick = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  useEffect(() => {
+    const handleEscapeKey = (e) => {
+      if (e.key === "Escape") {
+        setIsShow(false);
+        setIsSearchOpen(false); // Close search bar on escape key
+      }
+    };
+
+    const handleOutsideClick = (e) => {
+      if (!searchInput.current.contains(e.target)) {
+        setIsShow(false);
+        setIsSearchOpen(false); // Close search bar on outside click
+      }
+    };
+
+    const handleInputFocus = () => {
+      setIsShow(true);
+      setIsSearchOpen(true); // Open search bar on input focus
+    };
+
+    const handleInputBlur = () => {
+      setIsShow(false);
+    };
+
+    document.addEventListener("keydown", handleEscapeKey);
+    document.addEventListener("mousedown", handleOutsideClick);
+    searchInput.current.addEventListener("focus", handleInputFocus);
+    searchInput.current.addEventListener("blur", handleInputBlur);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+      document.removeEventListener("mousedown", handleOutsideClick);
+      searchInput.current.removeEventListener("focus", handleInputFocus);
+      searchInput.current.removeEventListener("blur", handleInputBlur);
+    };
+  }, []);
+
   return (
-    <Flex alignItems={"center"}>
-      <Stack direction={"row"} spacing={7}>
-        {/* Menu desktop */}
-        <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
-          {
-            navs.map((nav, index) => (
-              <Box key={index} role={'group'} transition={'all 0.3s ease'} pos={'relative'}>
-                <Box color="textColor" fontWeight={'bold'} _hover={{ color: "primaryColor" }}>
-                  <Link to={nav.href}>{nav.name}</Link>
-                </Box>
-                {
-                  nav.subs && (
-                    <>
-                      <Box pos='absolute' top={'25px'} left='0' bg={'#384e7b'} pl='10px' pr='30px' h='0' rounded={'md'}  opacity={0}
-                        _groupHover={{ h: '120px', opacity: '1', paddingY: '10px' }}
-                        transition={'all 0.3s ease'}
-                      >
-                        {
-                          nav.subs.map((sub, index) => (
-                            <Box key={index} w='max-content' fontWeight={'semibold'} transition={'.1s all'} color="textColor" letterSpacing={'1.7'} _hover={{ color: "primaryColor" }}>
-                              <Link to={sub.href}>{sub.name}</Link>
-                            </Box>
-                          ))
-                        }
-                      </Box>
-                    </>
-                  )
-                }
-              </Box>
-            ))
-          }
-        </HStack>
-        {/* Menu mobile */}
-        <MenuMobile navs={navs} />
-      </Stack>
-    </Flex>
-  )
-}
-
-const Navigation = () => {
-
-  return (
-    <Box zIndex="100" position={"relative"}>
+    <Box
+      w="100%"
+      position="relative"
+      bg="rgba(21, 31, 50, 1)"
+      zIndex={1000}
+      p="10px"
+    >
+      {/* Navigation Bar */}
       <Box
-        position="fixed"
-        top="0"
-        left="0"
-        right="0"
-        w="full"
-        zIndex="100"
-        boxShadow="rgba(0, 0, 0, 0.15) 0px 5px 15px"
-        bg={'primaryDarkColor'}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
       >
-        <Box layerStyle={"containerStyles"}>
-          <Flex
-            h={"55px"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-          >
-            {/* Logo  */}
-            <Box fontWeight="extrabold" color="primaryColor" fontSize="lg">
-              <Link to="/">MovieHubX</Link>
-            </Box>
+        {/* Logo and Navigation Links */}
+        <Box>
+          {/* Add your logo here */}
+          {/* Add your navigation links here */}
+        </Box>
 
-            {/* Input  */}
-            <NavInput />
-
-            {/* Menu */}
-            <Menu />
-          </Flex>
+        {/* Search Icon */}
+        <Box
+          fontSize="20px"
+          color="textColor"
+          cursor="pointer"
+          onClick={handleSearchIconClick}
+        >
+          <Search2Icon />
         </Box>
       </Box>
 
+      {/* Search Bar */}
+      <Box
+        position="absolute"
+        top="55px"  // Adjust the top position based on your navigation bar height
+        left="0"   // Align to the left
+        right="0"  // Take the whole width
+        bg="rgba(21, 31, 50, 1)"
+        zIndex={999}  // Lower zIndex to appear below navigation bar
+        p="10px"
+        transition="top 0.3s ease-in-out"  // Add transition effect
+        display={isSearchOpen ? "block" : "none"}  // Control display based on search bar open state
+      >
+        <Box position="relative" overflow="hidden">
+          <Input
+            variant="flushed"
+            autoCapitalize="off"
+            position="relative"
+            focusBorderColor="primaryColor"
+            placeholder="-_-"
+            _placeholder={{
+              color: "decsColor",
+            }}
+            value={searchText}
+            onChange={(e) => handleSearchTextChange(e)}
+            ref={searchInput}
+            fontSize={{
+              base: "sm",
+              md: "md",
+            }}
+            w="full"
+            pr="40px"
+            pl="5px"
+            onKeyDown={(e) => handlePressEnter(e)}
+          />
+          <Box
+            position="absolute"
+            right="15px"
+            top="50%"
+            transform="translateY(-50%)"
+            zIndex={500}
+            fontSize="20px"
+            color="textColor"
+            cursor="pointer"
+            onClick={() => handleSearchWithKeyWord()}
+          >
+            <Link to="/search">
+              <Search2Icon />
+            </Link>
+          </Box>
+        </Box>
+        {isShow && (
+          <SearchTopKeyWordsList
+            handleClickListKeyWords={handleSearchWithKeyWord}
+          />
+        )}
+      </Box>
     </Box>
   );
 };
 
-export default memo(Navigation);
+export default memo(NavInput);
