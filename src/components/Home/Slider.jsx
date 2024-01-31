14c1cc12fb794199b841807040241d8b21f6344a
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-31 19:54:10 +0200

    Update Slider.jsx

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -168,7 +168,7 @@ const Slider = () => {
                   <Box
                     pos={'absolute'} maxW={'600px'} zIndex={20} transform={'translateY(-50%)'}
                     mx={{ base: '10px', md: 'unset' }}
-                    top={{ base: '40%', md: '45%', }}
+                    top={{ base: '45%', md: '50%', }}  // Adjusted top position
                     left={{ base: '0', md: '130px', }}
                     right={{ base: '0', md: 'unset', }}
                   >
