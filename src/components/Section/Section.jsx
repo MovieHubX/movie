9500414607036e64db9ccad4f2e6ed207896f76a
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-20 21:15:20 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,7 @@
 import React, { Fragment, memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Keyboard } from "swiper";
-import "swiper/css";
-
-import ButtonBg from "../Buttons/ButtonBg";
 import Film from "../Film/Film";
 import { getConfigSelector } from "../../redux/selector";
 import { useSelector } from "react-redux";
@@ -17,7 +12,7 @@ const Section = ({ data = [], name, type, link = '' }) => {
 
   useEffect(() => {
     const calculateMoviesPerRow = () => {
-      const containerWidth = document.getElementById("swiper-container")?.offsetWidth;
+      const containerWidth = document.getElementById("section-container")?.offsetWidth;
       if (containerWidth) {
         const newMoviesPerRow = Math.floor(containerWidth / 200); // Assuming poster width of 200px
         setMoviesPerRow(newMoviesPerRow);
@@ -29,8 +24,31 @@ const Section = ({ data = [], name, type, link = '' }) => {
     return () => window.removeEventListener("resize", calculateMoviesPerRow);
   }, []);
 
+  const numRows = Math.ceil(data.length / moviesPerRow);
+  const movieRows = [];
+
+  for (let i = 0; i < numRows; i++) {
+    const moviesInRow = data.slice(i * moviesPerRow, (i + 1) * moviesPerRow);
+    movieRows.push(
+      <SimpleGrid key={i} columns={[1, 2, moviesPerRow]} spacing={6} mb={6}>
+        {moviesInRow.map((movie, j) => (
+          <Film
+            key={movie.id || j}
+            baseUrl={`${config?.images?.base_url}/original/`}
+            media_type={type}
+            id={movie.id}
+            vote_average={movie.vote_average}
+            poster_path={movie.poster_path}
+            title={movie.title}
+            name={movie.name}
+          />
+        ))}
+      </SimpleGrid>
+    );
+  }
+
   return (
-    <Box mb="50px">
+    <Box mb="50px" id="section-container">
       <Flex mb="30px" justify="space-between" align="center">
         <Heading
           textTransform="capitalize"
@@ -42,112 +60,16 @@ const Section = ({ data = [], name, type, link = '' }) => {
         >
           {data?.homeSectionName || name}
         </Heading>
-        {
-          link && (
-            <Link to={link}>
-              <ButtonBg>
-                More
-                <ArrowForwardIcon ml={2} />
-              </ButtonBg>
-            </Link>
-          )
-        }
+        {link && (
+          <Link to={link}>
+            <ButtonBg>
+              More
+              <ArrowForwardIcon ml={2} />
+            </ButtonBg>
+          </Link>
+        )}
       </Flex>
-
-      <div className="swiper-container">
-        <Swiper
-          slidesPerView={moviesPerRow}
-          spaceBetween={15}
-          breakpoints={{
-            768: {
-              slidesPerView: Math.min(moviesPerRow, 4.3),
-            },
-            922: {
-              slidesPerView: Math.min(moviesPerRow, 6.3),
-            },
-          }}
-          keyboard={true}
-          modules={[Keyboard]}
-        >
-          {data.map((movie, i) => (
-            <SwiperSlide key={movie.id || i}>
-              <Film
-                baseUrl={`${config?.images?.base_url}/original/`}
-                media_type={type}
-                id={movie.id}
-                vote_average={movie.vote_average}
-                poster_path={movie.poster_path}
-                title={movie.title}
-                name={movie.name}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      {/* Second Row */}
-      <div className="swiper-container">
-        <Swiper
-          slidesPerView={moviesPerRow}
-          spaceBetween={15}
-          breakpoints={{
-            768: {
-              slidesPerView: Math.min(moviesPerRow, 4.3),
-            },
-            922: {
-              slidesPerView: Math.min(moviesPerRow, 6.3),
-            },
-          }}
-          keyboard={true}
-          modules={[Keyboard]}
-        >
-          {data.map((movie, i) => (
-            <SwiperSlide key={movie.id || i}>
-              <Film
-                baseUrl={`${config?.images?.base_url}/original/`}
-                media_type={type}
-                id={movie.id}
-                vote_average={movie.vote_average}
-                poster_path={movie.poster_path}
-                title={movie.title}
-                name={movie.name}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      {/* Third Row */}
-      <div className="swiper-container">
-        <Swiper
-          slidesPerView={moviesPerRow}
-          spaceBetween={15}
-          breakpoints={{
-            768: {
-              slidesPerView: Math.min(moviesPerRow, 4.3),
-            },
-            922: {
-              slidesPerView: Math.min(moviesPerRow, 6.3),
-            },
-          }}
-          keyboard={true}
-          modules={[Keyboard]}
-        >
-          {data.map((movie, i) => (
-            <SwiperSlide key={movie.id || i}>
-              <Film
-                baseUrl={`${config?.images?.base_url}/original/`}
-                media_type={type}
-                id={movie.id}
-                vote_average={movie.vote_average}
-                poster_path={movie.poster_path}
-                title={movie.title}
-                name={movie.name}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {movieRows}
     </Box>
   );
 };
