719c6e72246ee02c61090ca1a58d049538d57dbc
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-21 13:18:38 +0200

    Update SectionTrending.jsx

--- src/components/Section/SectionTrending.jsx
+++ src/components/Section/SectionTrending.jsx
@@ -1,93 +1,106 @@
-import React, { useState } from "react";
+import React, { Fragment, memo, useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import { ArrowForwardIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { useSprings, animated } from "react-spring";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard } from "swiper";
 import "swiper/css";
-import ButtonBg from "../Buttons/ButtonBg";
-import Film from "../Film/Film";
 import { getConfigSelector } from "../../redux/selector";
 import { useSelector } from "react-redux";
+
+import Film from "../Film/Film";
+import ButtonBg from "../Buttons/ButtonBg";
 
 const SectionTrending = ({ data = [], name, trendingInWeek, setTrendingInWeek }) => {
   const { config } = useSelector(getConfigSelector);
   const [progress, setProgress] = useState(0);
 
-  const handleNext = () => {
+  const handleSlideChange = (swiper) => {
+    setProgress(swiper.activeIndex);
+  };
+
+  const handleNext = (swiper) => {
     if (progress < data.length - 1) {
-      setProgress(prev => prev + 1);
+      swiper.slideNext();
     }
   };
 
-  const handlePrev = () => {
+  const handlePrev = (swiper) => {
     if (progress > 0) {
-      setProgress(prev => prev - 1);
+      swiper.slidePrev();
     }
   };
-
-  const progressSprings = useSprings(
-    data.length,
-    data.map((_, index) => ({
-      width: `${(index <= progress ? 1 : 0) * 100}%`,
-    }))
-  );
 
   return (
     <Box mb="50px" position="relative">
       <Flex mb="30px" justify="space-between" align="center">
-        {/* heading */}
-        <Box>
-          <Heading
-            textTransform="capitalize"
-            fontSize={{
-              base: "xl",
-              md: "2xl",
-              lg: "3xl",
-            }}
-          >
-            {data?.homeSectionName || name}
-          </Heading>
-        </Box>
-        {/* change time */}
-        <Flex
-          overflow={"hidden"}
-          display={{ base: "none", md: "flex" }}
-          rounded="3xl"
-          border={"rgba(50, 138, 241, 1) 1px solid"}
-          justify={"space-between"}
-          align="center"
-          w={"234px"}
-          py="5px"
-          fontWeight="bold"
-          color="#fff"
-          position="relative"
-          cursor="pointer"
-          textAlign={"center"}
-        >
-          {/* This Week */}
-          <Box
-            w={"50%"}
-            onClick={() => setTrendingInWeek(prev => !prev)}
-            color={trendingInWeek ? "#fff" : "#63b3ed"}
-            pos={"relative"}
-            bg="transparent"
-            zIndex={1}
-          >
-            This Week
+        <Flex justify={"center"} align="center" columnGap={"8"}>
+          {/* heading */}
+          <Box>
+            <Heading
+              textTransform="capitalize"
+              fontSize={{
+                base: "xl",
+                md: "2xl",
+                lg: "3xl",
+              }}
+            >
+              {data?.homeSectionName || name}
+            </Heading>
           </Box>
-          {/* Today */}
-          <Box
-            w={"50%"}
-            onClick={() => setTrendingInWeek(prev => !prev)}
-            color={trendingInWeek ? "#63b3ed" : "#fff"}
-            pos={"relative"}
-            bg="transparent"
-            zIndex={1}
+          {/* change time */}
+          <Flex
+            overflow={"hidden"}
+            display={{ base: "none", md: "flex" }}
+            rounded="3xl"
+            border={"rgba(50, 138, 241, 1) 1px solid"}
+            justify={"space-between"}
+            align="center"
+            w={"234px"}
+            py="5px"
+            fontWeight="bold"
+            color="#fff"
+            position="relative"
+            cursor="pointer"
+            textAlign={"center"}
           >
-            Today
-          </Box>
+            {/* This Week */}
+            <Box
+              w={"50%"}
+              onClick={() => setTrendingInWeek((prev) => !prev)}
+              color={trendingInWeek ? "#fff" : "#63b3ed"}
+              pos={"relative"}
+              bg="transparent"
+              zIndex={1}
+            >
+              This Week
+            </Box>
+            {/* Today */}
+            <Box
+              w={"50%"}
+              onClick={() => setTrendingInWeek((prev) => !prev)}
+              pos={"relative"}
+              color={trendingInWeek ? "#63b3ed" : "#fff"}
+              bg="transparent"
+              zIndex={1}
+            >
+              Today
+            </Box>
+            <motion.div
+              initial="week"
+              variants={{ week: { left: 0 }, day: { left: "50%" } }}
+              transition={{ type: "spring", stiffness: 100, damping: 20 }}
+              animate={trendingInWeek ? "week" : "day"}
+              style={{
+                position: "absolute",
+                top: 0,
+                height: "100%",
+                width: "50%",
+                backgroundColor: trendingInWeek ? "#3182ce" : "#63b3ed",
+                borderRadius: "3xl",
+              }}
+            ></motion.div>
+          </Flex>
         </Flex>
         <Link to={`/trending/${trendingInWeek ? "week" : "day"}`}>
           <ButtonBg>
@@ -100,17 +113,14 @@ const SectionTrending = ({ data = [], name, trendingInWeek, setTrendingInWeek })
       {/* Progress bar container */}
       <Box mb="20px">
         <Box display="flex" bg="#fff" borderRadius="2px" overflow="hidden">
-          {progressSprings.map((style, index) => (
-            <animated.div
+          {data.map((_, index) => (
+            <Box
               key={index}
-              style={{
-                ...style,
-                height: "4px",
-                flex: 1,
-                mx: "-1px",
-                borderRadius: 0,
-                backgroundColor: "#3182ce",
-              }}
+              bg={index <= progress ? "#3182ce" : "#cbd5e0"}
+              h="4px"
+              flex="1"
+              mx="-1px" // Adjusted margin to remove white space between dots
+              borderRadius="0" // Removed border radius to make it seamless
             />
           ))}
         </Box>
@@ -126,39 +136,37 @@ const SectionTrending = ({ data = [], name, trendingInWeek, setTrendingInWeek })
         }}
         keyboard={true}
         modules={[Keyboard]}
-        onSlideChange={(swiper) => setProgress(swiper.activeIndex)}
+        onSlideChange={handleSlideChange}
       >
-        <Box position="relative">
-          <SwiperSlide>
-            <SimpleGrid columns={[1, 2, 4]} spacing={6}>
-              {data.map((data, i) => {
-                if (i < 18) {
-                  return (
-                    <Film
-                      key={data.id}
-                      baseUrl={`${config?.images?.base_url}/original/`}
-                      media_type={data.media_type}
-                      id={data.id}
-                      vote_average={data.vote_average}
-                      poster_path={data.poster_path}
-                      title={data.title}
-                      name={data.name}
-                    />
-                  );
-                }
-                return null;
-              })}
-            </SimpleGrid>
-          </SwiperSlide>
-          {/* Arrow buttons */}
-          <Box position="absolute" top="50%" left="0" zIndex={1} onClick={handlePrev}>
-            <ChevronLeftIcon boxSize={12} color="blue.500" cursor="pointer" />
-          </Box>
-          <Box position="absolute" top="50%" right="0" zIndex={1} onClick={handleNext}>
-            <ChevronRightIcon boxSize={12} color="blue.500" cursor="pointer" />
-          </Box>
-        </Box>
+        <SimpleGrid columns={[1, 2, 4]} spacing={6}>
+          {data?.map((data, i) => {
+            if (i < 18) {
+              return (
+                <SwiperSlide key={data.id}>
+                  <Film
+                    baseUrl={`${config?.images?.base_url}/original/`}
+                    media_type={data.media_type}
+                    id={data.id}
+                    vote_average={data.vote_average}
+                    poster_path={data.poster_path}
+                    title={data.title}
+                    name={data.name}
+                  />
+                </SwiperSlide>
+              );
+            }
+            return <Fragment key={data.id || i}></Fragment>;
+          })}
+        </SimpleGrid>
       </Swiper>
+
+      {/* Arrow buttons */}
+      <Box position="absolute" top="50%" transform="translateY(-50%)" left="0" zIndex={1}>
+        <ChevronLeftIcon boxSize={12} color="blue.500" cursor="pointer" onClick={() => handlePrev(swiper)} />
+      </Box>
+      <Box position="absolute" top="50%" transform="translateY(-50%)" right="0" zIndex={1}>
+        <ChevronRightIcon boxSize={12} color="blue.500" cursor="pointer" onClick={() => handleNext(swiper)} />
+      </Box>
     </Box>
   );
 };
