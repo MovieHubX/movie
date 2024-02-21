639739a2bd34fff69d402432d50cd3e4cfb51ac2
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-21 22:05:56 +0200

    Update SectionTrending.jsx

--- src/components/Section/SectionTrending.jsx
+++ src/components/Section/SectionTrending.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, memo, useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Flex, Heading, SimpleGrid, Button, Stack } from "@chakra-ui/react";
+import { Box, Flex, Heading, SimpleGrid, Button } from "@chakra-ui/react";
 import { ArrowForwardIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard } from "swiper";
@@ -49,19 +49,14 @@ const SectionTrending = ({ data = [], name, trendingInWeek, setTrendingInWeek })
           {data?.homeSectionName || name}
         </Heading>
         {/* switcher */}
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          spacing={{ base: "4", md: "2" }}
+        <Button
+          size="sm"
+          onClick={() => setTrendingInWeek(prev => !prev)}
+          variant={trendingInWeek ? "solid" : "outline"}
+          colorScheme={trendingInWeek ? "blue" : "gray"}
         >
-          <Button
-            size="sm"
-            onClick={() => setTrendingInWeek(prev => !prev)}
-            variant={trendingInWeek ? "solid" : "outline"}
-            colorScheme={trendingInWeek ? "blue" : "gray"}
-          >
-            {trendingInWeek ? "Weekly ‏‏‎‎" : "Daily‎‎‎‎‏‏‎ "} Trending
-          </Button>
-        </Stack>
+          {trendingInWeek ? "Weekly ‏‏‎‎" : "Daily‎‎‎‎‏‏‎ "} Trending
+        </Button>
         {/* more button */}
         <Link to={`/trending/${trendingInWeek ? "week" : "day"}`}>
           <ButtonBg>
