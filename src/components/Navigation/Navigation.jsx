d6f1ccaec18f9466f194db243a2b4d15cb5f6db9
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-22 13:21:00 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from "react";
 import { Link } from "react-router-dom";
 
-import { Box, Flex, HStack, Spacer } from "@chakra-ui/react";
+import { Box, Flex, HStack, Stack } from "@chakra-ui/react";
 import MenuMobile from "./MenuMobile";
 import NavInput from "./NavInput";
 import { navs } from "../Constans";
@@ -9,40 +9,43 @@ import { navs } from "../Constans";
 const Menu = () => {
   return (
     <Flex alignItems={"center"}>
-      <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
-        {navs.map((nav, index) => (
-          <Box key={index} role={"group"} transition={"all 0.3s ease"} pos={"relative"}>
-            <Box color="textColor" fontWeight={"bold"} _hover={{ color: "primaryColor" }}>
-              <Link to={nav.href}>{nav.name}</Link>
+      <Stack direction={"row"} spacing={7}>
+        {/* Menu desktop */}
+        <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
+          {navs.map((nav, index) => (
+            <Box key={index} role={"group"} transition={"all 0.3s ease"} pos={"relative"}>
+              <Box color="textColor" fontWeight={"bold"} _hover={{ color: "primaryColor" }}>
+                <Link to={nav.href}>{nav.name}</Link>
+              </Box>
+              {nav.subs && (
+                <>
+                  <Box
+                    pos="absolute"
+                    top={"25px"}
+                    left="0"
+                    bg={"#384e7b"}
+                    pl="10px"
+                    pr="30px"
+                    h="0"
+                    rounded={"md"}
+                    opacity={0}
+                    _groupHover={{ h: "120px", opacity: "1", paddingY: "10px" }}
+                    transition={"all 0.3s ease"}
+                  >
+                    {nav.subs.map((sub, index) => (
+                      <Box key={index} w="max-content" fontWeight={"semibold"} transition={".1s all"} color="textColor" letterSpacing={"1.7"} _hover={{ color: "primaryColor" }}>
+                        <Link to={sub.href}>{sub.name}</Link>
+                      </Box>
+                    ))}
+                  </Box>
+                </>
+              )}
             </Box>
-            {nav.subs && (
-              <>
-                <Box
-                  pos="absolute"
-                  top={"25px"}
-                  left="0"
-                  bg={"#384e7b"}
-                  pl="10px"
-                  pr="30px"
-                  h="0"
-                  rounded={"md"}
-                  opacity={0}
-                  _groupHover={{ h: "120px", opacity: "1", paddingY: "10px" }}
-                  transition={"all 0.3s ease"}
-                >
-                  {nav.subs.map((sub, index) => (
-                    <Box key={index} w="max-content" fontWeight={"semibold"} transition={".1s all"} color="textColor" letterSpacing={"1.7"} _hover={{ color: "primaryColor" }}>
-                      <Link to={sub.href}>{sub.name}</Link>
-                    </Box>
-                  ))}
-                </Box>
-              </>
-            )}
-          </Box>
-        ))}
-      </HStack>
-      {/* Menu mobile */}
-      <MenuMobile navs={navs} />
+          ))}
+        </HStack>
+        {/* Menu mobile */}
+        <MenuMobile navs={navs} />
+      </Stack>
     </Flex>
   );
 };
@@ -67,14 +70,8 @@ const Navigation = () => {
               <Link to="/">MovieHubX</Link>
             </Box>
 
-            {/* Spacer for space between logo and search icon */}
-            <Spacer />
-
-            {/* Search Input */}
+            {/* Input */}
             <NavInput />
-
-            {/* Spacer for space between search icon and nav links */}
-            <Spacer />
 
             {/* Menu */}
             <Menu />
