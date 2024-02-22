a847dc7d2e09cf5c992240597b06cdb9330b6693
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-22 13:11:26 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from "react";
 import { Link } from "react-router-dom";
 
-import { Box, Flex, HStack, Stack } from "@chakra-ui/react";
+import { Box, Flex, HStack, Spacer } from "@chakra-ui/react";
 import MenuMobile from "./MenuMobile";
 import NavInput from "./NavInput";
 import { navs } from "../Constans";
@@ -9,43 +9,40 @@ import { navs } from "../Constans";
 const Menu = () => {
   return (
     <Flex alignItems={"center"}>
-      <Stack direction={"row"} spacing={7}>
-        {/* Menu desktop */}
-        <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
-          {navs.map((nav, index) => (
-            <Box key={index} role={"group"} transition={"all 0.3s ease"} pos={"relative"}>
-              <Box color="textColor" fontWeight={"bold"} _hover={{ color: "primaryColor" }}>
-                <Link to={nav.href}>{nav.name}</Link>
-              </Box>
-              {nav.subs && (
-                <>
-                  <Box
-                    pos="absolute"
-                    top={"25px"}
-                    left="0"
-                    bg={"#384e7b"}
-                    pl="10px"
-                    pr="30px"
-                    h="0"
-                    rounded={"md"}
-                    opacity={0}
-                    _groupHover={{ h: "120px", opacity: "1", paddingY: "10px" }}
-                    transition={"all 0.3s ease"}
-                  >
-                    {nav.subs.map((sub, index) => (
-                      <Box key={index} w="max-content" fontWeight={"semibold"} transition={".1s all"} color="textColor" letterSpacing={"1.7"} _hover={{ color: "primaryColor" }}>
-                        <Link to={sub.href}>{sub.name}</Link>
-                      </Box>
-                    ))}
-                  </Box>
-                </>
-              )}
+      <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
+        {navs.map((nav, index) => (
+          <Box key={index} role={"group"} transition={"all 0.3s ease"} pos={"relative"}>
+            <Box color="textColor" fontWeight={"bold"} _hover={{ color: "primaryColor" }}>
+              <Link to={nav.href}>{nav.name}</Link>
             </Box>
-          ))}
-        </HStack>
-        {/* Menu mobile */}
-        <MenuMobile navs={navs} />
-      </Stack>
+            {nav.subs && (
+              <>
+                <Box
+                  pos="absolute"
+                  top={"25px"}
+                  left="0"
+                  bg={"#384e7b"}
+                  pl="10px"
+                  pr="30px"
+                  h="0"
+                  rounded={"md"}
+                  opacity={0}
+                  _groupHover={{ h: "120px", opacity: "1", paddingY: "10px" }}
+                  transition={"all 0.3s ease"}
+                >
+                  {nav.subs.map((sub, index) => (
+                    <Box key={index} w="max-content" fontWeight={"semibold"} transition={".1s all"} color="textColor" letterSpacing={"1.7"} _hover={{ color: "primaryColor" }}>
+                      <Link to={sub.href}>{sub.name}</Link>
+                    </Box>
+                  ))}
+                </Box>
+              </>
+            )}
+          </Box>
+        ))}
+      </HStack>
+      {/* Menu mobile */}
+      <MenuMobile navs={navs} />
     </Flex>
   );
 };
@@ -70,16 +67,14 @@ const Navigation = () => {
               <Link to="/">MovieHubX</Link>
             </Box>
 
+            {/* Center content */}
+            <NavInput />
+
+            {/* Spacer for space between input and nav links */}
+            <Spacer />
+
             {/* Right side content */}
-            <Box>
-              <Flex alignItems="center">
-                {/* Menu */}
-                <Menu />
-                
-                {/* Input */}
-                <NavInput />
-              </Flex>
-            </Box>
+            <Menu />
           </Flex>
         </Box>
       </Box>
