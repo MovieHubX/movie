cceea9a4618c15c338f243305f18923897333ba6
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-22 17:28:34 +0200

    Update MovieDetail.jsx

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -178,7 +178,7 @@ export const MovieDetail = () => {
             ref={player}
           >
             <VideoPlayer
-              embedSrc={`https://blackvid.space/embed/movie?tmdb=${id}`}
+              embedSrc={`https://blackvid.space/embed?tmdb=${id}`}
             />
           </Box>
 
