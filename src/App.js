811d29d044fa004ba826e2b61d1448de13c33067
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-23 13:03:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "src/components/Layout/Layout";
-// import Layout from "./components/Layout/Layout";
-
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import {
@@ -48,14 +46,14 @@ function App() {
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/search" element={<Search />}></Route>
 
-        <Route exact path="/movie" element={<Movie />}></Route>
+        <Route exact path="/movie" element={<Movie type='popular' />}></Route>
         <Route exact path="/movie/popular" element={<Movie type='popular' />}></Route>
         <Route exact path="/movie/now-playing" element={<Movie type='now_playing' />}></Route>
         <Route exact path="/movie/upcoming" element={<Movie type='upcoming' />}></Route>
         <Route exact path="/movie/top-rated" element={<Movie type='top_rated' />}></Route>
         <Route exact path="/movie/:id" element={<MovieDetail />}></Route>
 
-        <Route exact path="/tv" element={<Tv />}></Route>
+        <Route exact path="/tv" element={<Tv type='popular' />}></Route>
         <Route exact path="/tv/popular" element={<Tv type='popular' />}></Route>
         <Route exact path="/tv/airing-today" element={<Tv type='airing_today' />}></Route>
         <Route exact path="/tv/on-the-air" element={<Tv type='on_the_air' />}></Route>
@@ -65,7 +63,6 @@ function App() {
 
         <Route exact path="/trending/week" element={<TrendingWeek />}></Route>
         <Route exact path="/trending/day" element={<TrendingDay />}></Route>
-
 
         <Route path='*' exact={true} element={<NotFound />} />
       </Routes>
