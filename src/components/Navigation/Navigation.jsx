16948239ffdfac9fff9d9d8f45dfa20bebde1d94
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-23 14:39:35 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -71,7 +71,7 @@ const Navigation = () => {
             </Box>
 
             {/* Input with margin */}
-            <NavInput ml={1} />
+            <NavInput ml={5} />
 
             {/* Wrapped Menu with margin */}
             <Box ml={5} mt={1}> {/* Added mt={2} for moving it down */}
