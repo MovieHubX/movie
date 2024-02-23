369e454aa2b27e68968b6981466a7786994f3d44
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-23 17:09:17 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -9,7 +9,7 @@ import { navs } from "../Constans";
 const Menu = () => {
   return (
     <Flex alignItems={"center"}>
-      <Stack direction={"row"} spacing={7}>
+      <Stack direction={"row"} spacing={7} ml={1}>
         {/* Menu desktop */}
         <HStack spacing="30px" display={{ base: "none", lg: "flex" }}>
           {navs.map((nav, index) => (
@@ -70,11 +70,13 @@ const Navigation = () => {
               <Link to="/">MovieHubX</Link>
             </Box>
 
-            {/* Input */}
-            <NavInput />
+            {/* Input with margin */}
+            <NavInput ml={1} />
 
-            {/* Menu */}
-            <Menu />
+            {/* Wrapped Menu with margin */}
+            <Box ml={5} mt={2}> {/* Added mt={2} for moving it down */}
+              <Menu />
+            </Box>
           </Flex>
         </Box>
       </Box>
