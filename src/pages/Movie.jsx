566fa03fa25a09f3863bcbaec69835791ee42b8e
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-02-25 13:08:06 +0200

    Update Movie.jsx

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,66 +1,51 @@
-import React, { useCallback, useEffect, useRef } from 'react';
-import { Box, Flex, Heading } from '@chakra-ui/react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getMovieSelector } from 'src/redux/selector';
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import React, { useCallback, useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+
+import { useDispatch } from 'react-redux'
+import ListFilmInfinity from 'src/components/Layout/ListFilmInfinity'
+import { getMovieSelector } from 'src/redux/selector'
+import { fetchMoviesData } from 'src/services'
 
 const HeadingLookup = {
   popular: 'Popular Movies',
-  now_playing: 'Now Playing Movies',
+  'now_playing': 'Now Playing Movies',
   upcoming: 'Upcoming Movies',
-  top_rated: 'Top Rated Movies'
-};
-
+  'top_rated': 'Top Rated Movies'
+}
 export const Movie = ({ type = 'popular' }) => {
-  const dispatch = useDispatch();
-  let pageCount = useRef(0);
-  const { data: movies, status, page } = useSelector(getMovieSelector);
-  const { config } = useSelector(getConfigSelector);
-
+  const dispatch = useDispatch()
+  let pageCount = useRef(0)
+  const { value, status, page } = useSelector(getMovieSelector);
   const handleDispatchAction = useCallback(() => {
-    // Your dispatch action here if required
-    // dispatch(fetchMoviesData({ type, page: ++pageCount.current }));
+    dispatch(
+      fetchMoviesData({
+        path: `movie/${type}`,
+        params: { page: ++pageCount.current },
+      })
+    );
   }, [dispatch, type]);
-
+    
   useEffect(() => {
     pageCount.current = 0;
     handleDispatchAction();
     window.scrollTo(0, 0);
   }, [type, handleDispatchAction]);
-
   return (
     <Box mt={'50px'}>
       <Flex mb="30px" justify="space-between" align="center">
         <Heading
           textTransform="capitalize"
           fontSize={{
-            base: 'xl',
-            md: '2xl',
-            lg: '3xl'
-          }}>
+            base: "xl",
+            md: "2xl",
+            lg: "3xl",
+          }}
+        >
           {HeadingLookup[type]}
         </Heading>
       </Flex>
-      <SimpleGrid
-        columns={{ base: 2, sm: 2, md: 4, lg: 6 }}
-        spacing="4"
-        sx={{
-          '@media (max-width: 480px)': {
-            gridTemplateColumns: 'repeat(3, 1fr)',
-          },
-        }}>
-        {movies?.map((movie, i) => (
-          <Film
-            key={movie.id}
-            baseUrl={`${config?.images?.base_url}/original/`}
-            media_type={type}
-            id={movie.id}
-            vote_average={movie.vote_average}
-            poster_path={movie.poster_path}
-            title={movie.title}
-            name={movie.name}
-          />
-        ))}
-      </SimpleGrid>
+      <ListFilmInfinity listFilm={value || []} status={status} pageCount={page} dispatchAction={handleDispatchAction} />
     </Box>
-  );
-};
+  )
+}
